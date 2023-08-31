ad496efd3b26b0353b26a814248abd3a22bc9037
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-08-31 15:33:07 +0100

    Chat finished

--- SignalRBasics/Controllers/HomeController.cs
+++ SignalRBasics/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SignalRBasics.Controllers
             return View();
         }
 
+        public ViewResult ChatDemo()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
