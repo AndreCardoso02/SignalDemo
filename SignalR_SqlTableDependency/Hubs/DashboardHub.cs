da3b4d6ddaef32a313aaa8a95d2830cb74e64f97
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-09-05 10:36:26 +0100

    Configuring events in Dashboard Hub

--- SignalR_SqlTableDependency/Hubs/DashboardHub.cs
+++ SignalR_SqlTableDependency/Hubs/DashboardHub.cs
@@ -21,18 +21,27 @@ namespace SignalR_SqlTableDependency.Hubs
         {
             var products = productRepository.GetProducts();
             await Clients.All.SendAsync("ReceivedProducts", products);
+
+            var productsForGraph = productRepository.GetProductsForGraph();
+            await Clients.All.SendAsync("ReceivedProductsForGraph", productsForGraph);
         }
 
         public async Task SendSales()
         {
             var sales = saleRepository.GetSales();
             await Clients.All.SendAsync("ReceivedSales", sales);
+
+            var salesForGraph = saleRepository.GetSalesForGraph();
+            await Clients.All.SendAsync("ReceivedSalesForGraph", salesForGraph);
         }
 
         public async Task SendCustomers()
         {
             var customers = customerRepository.GetCustomers();
             await Clients.All.SendAsync("ReceivedCustomers", customers);
+
+            var customersForGraph = customerRepository.GetCustomersForGraph();
+            await Clients.All.SendAsync("ReceivedCustomersForGraph", customersForGraph);
         }
     }
 }
