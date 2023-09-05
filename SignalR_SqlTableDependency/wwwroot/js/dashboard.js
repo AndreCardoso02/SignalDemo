07f5a52c36fc5307f45c40b5bfb41e6c79e1b65c
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-09-05 11:46:28 +0100

    Receiving and building graphics with javascript (dashboard.js)

--- SignalR_SqlTableDependency/wwwroot/js/dashboard.js
+++ SignalR_SqlTableDependency/wwwroot/js/dashboard.js
@@ -14,7 +14,6 @@ $(function () {
     });
 });
 
-
 // -- Send and Receive Products List
 function InvokeProducts() {
     connection.invoke("SendProducts").catch(function (err) {
@@ -37,6 +36,44 @@ function BindProductsToGrid(products) {
         tr.append(`<td>${product.category}</td>`);
         tr.append(`<td>${product.price}</td>`);
         $('#tblProduct').append(tr);
+    });
+}
+
+connection.on("ReceivedProductsForGraph", function (productsForGraph) {
+    BindProductsToGraph(productsForGraph);
+});
+
+function BindProductsToGraph(productsForGraph) {
+    var labels = [];
+    var data = [];
+
+    $.each(productsForGraph, function (index, item) {
+        labels.push(item.category);
+        data.push(item.products);
+    });
+
+    DestroyCanvasIfExists('canvasProducts');
+
+    const context = $('#canvasProducts');
+    const myChart = new Chart(context, {
+        type: 'doughnut',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '# of Products',
+                data: data,
+                backgroundColor: backgroundColors,
+                borderColor: borderColors,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
     });
 }
 
@@ -66,6 +103,44 @@ function BindSalesToGrid(sales) {
     });
 }
 
+connection.on("ReceivedSalesForGraph", function (salesForGraph) {
+    BindSalesToGraph(salesForGraph);
+});
+
+function BindSalesToGraph(salesForGraph) {
+    var labels = [];
+    var data = [];
+
+    $.each(salesForGraph, function (index, item) {
+        labels.push(item.purchasedOn);
+        data.push(item.amount);
+    });
+
+    DestroyCanvasIfExists('canvasSales');
+
+    const context = $('#canvasSales');
+    const myChart = new Chart(context, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '# of Sales',
+                data: data,
+                backgroundColor: backgroundColors,
+                borderColor: borderColors,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
 // Send and Receive Customers
 function InvokeCustomers() {
     connection.invoke("SendCustomers").catch(function (err) {
@@ -92,3 +167,69 @@ function BindCustomersToGrid(customers) {
         $('#tblCustomer').append(tr);
     });
 }
+
+connection.on("ReceivedCustomersForGraph", function (customersForGraph) {
+    BindCustomersToGraph(customersForGraph);
+});
+
+function BindCustomersToGraph(cutomersForGraph) {
+    var datasets = [];
+    var labels = ['Customers'];
+    var data = [];
+
+    $.each(cutomersForGraph, function (index, item) {
+        data = [];
+        data.push(item.customers);
+        var dataset = {
+            label: item.gender,
+            data: data,
+            backgroundColor: backgroundColors[index],
+            borderColor: borderColors[index],
+            borderWidth: 1
+        };
+
+        datasets.push(dataset);
+    });
+
+    DestroyCanvasIfExists('canvasCustomers');
+
+    const context = $('#canvasCustomers');
+    const myChart = new Chart(context, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: datasets
+        },
+        options: {
+            scales: {
+                beginAtZero: true
+            }
+        }
+    });
+}
+
+// Extra  functions for graphs
+function DestroyCanvasIfExists(canvasId) {
+    let chartStatus = Chart.getChart(canvasId);
+    if (chartStatus != undefined) {
+        chartStatus.destroy();
+    }
+}
+
+var backgroundColors = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)'
+];
+
+var borderColors = [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
+];
